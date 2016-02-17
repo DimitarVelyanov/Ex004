88b03a7ad6385af9c94f3cc876603dc33ca93f5c
d_velqnov <d_velqnov@abv.bg>
2016-02-17 17:17:04 +0200

    .

---
+++ src/Maine.java
@@ -0,0 +1,43 @@
+
+public class Maine {
+
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 4;
+		System.out.print("a=");
+		System.out.println(a);
+
+		System.out.print("b=");
+		System.out.println(b);
+		
+		boolean aEqualsB =a==b;
+		System.out.print("a==b:");
+		System.out.println(aEqualsB);
+		
+		boolean aDiffersB=a!=b;
+		System.out.print("a!=b:");
+		System.out.println(aDiffersB);
+		
+		boolean aIsLessThan= a<b;
+		System.out.print("a<b:");
+		System.out.println(aIsLessThan);
+		
+		boolean aIsGreaterThanB = a>b;
+		System.out.print("a>b:");
+		System.out.println(aIsGreaterThanB);
+		
+		boolean aIsLessOrEqualThanB =a<=b;
+		System.out.print("=a<=b:");
+		System.out.println(aIsLessOrEqualThanB);
+		
+		boolean aIsGreaterOrEqualThanB =a>=b;
+		System.out.print("=a>=b:");
+		System.out.println(aIsGreaterOrEqualThanB);
+		
+		
+		
+		
+
+	}
+
+}
